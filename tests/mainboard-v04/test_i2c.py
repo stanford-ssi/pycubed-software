81973b8691e21caea7cabcd72d620fdbf9bc7b37
Flynn Dreilinger <flynnd@stanford.edu>
2021-04-13 01:28:34 -0700

    add lock test and rgb sensor test

--- tests/mainboard-v04/test_i2c.py
+++ tests/mainboard-v04/test_i2c.py
@@ -1,7 +1,10 @@
-import unittest
-
 import busio
 import board
+import time
+
+import unittest
+import adafruit_tcs34725
+
 
 class TestI2C(unittest.TestCase):
 
@@ -9,9 +12,32 @@ class TestI2C(unittest.TestCase):
     Test a simple locking of a device two possible I2C pins. Here we tested an
     adafruit TCS34725 RGB sensor with the red green and blue LEDS.
     """
-    def simple_test(self):
+
+    def test_lock(self):
+        print("") # XXX newline
         # busio.I2C(scl, sda) respectively
         i2c = busio.I2C(board.SCK, board.MOSI)
         i2c.try_lock()
-        i2c.scan()
+        scan = i2c.scan()
+        print("I2C scan:")
+        print(scan)
         print("Locked Successfully!")
+        i2c.unlock()
+        print("Unlocked Successfully!")
+        print("----------")
+        i2c.deinit()
+
+    def test_rgb_sensor(self):
+        print("") # XXX newline
+        # busio.I2C(scl, sda) respectively
+        i2c = busio.I2C(board.SCK, board.MOSI)
+        sensor = adafruit_tcs34725.TCS34725(i2c)
+        # read the color temperature and lux of the sensor too
+        for i in range(3):
+            temp = sensor.color_temperature
+            lux = sensor.lux
+            print("Temperature: {0}K Lux: {1}".format(temp, lux))
+            time.sleep(1.0)
+
+        print("----------")
+        i2c.deinit()
