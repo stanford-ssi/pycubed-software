23216034f71599e63d4d7dca5c1d18948d2b9223
Flynn Dreilinger <flynnd@stanford.edu>
2021-04-13 00:49:18 -0700

    add rgb test

--- tests/mainboard-v04/test_neopixel.py
+++ tests/mainboard-v04/test_neopixel.py
@@ -1,4 +1,12 @@
+# standard libraries
+import time
+
+# 3rd-party libraries
 import unittest
+
+# pycubed libraries
+from pycubed import cubesat
+
 
 class TestNeoPixel(unittest.TestCase):
 
@@ -6,15 +14,20 @@ class TestNeoPixel(unittest.TestCase):
     NeoPixel unit tests; runs red, green, and blue lights at low brightness three
     times Note: Neopixel can go from (R,G,B) values from 0 to 255.
     """
-    def Test_NeoPixel(self):
+
+    def test_rgb(self):
+        print("\n")
         for i in range(3):
-            print("PyCubed Running!")
+            print("Red")
             cubesat.RGB = (50, 0, 0)
             time.sleep(1)
+            print("Green")
             cubesat.RGB = (0, 50, 0)
             time.sleep(1)
+            print("Blue")
             cubesat.RGB = (0, 0, 50)
             time.sleep(1)
             self.assertEqual(cubesat.RGB[0], 0)
             self.assertEqual(cubesat.RGB[1], 0)
             self.assertEqual(cubesat.RGB[2], 50)
+        print("\n---------------------")
